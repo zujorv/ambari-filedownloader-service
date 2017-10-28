e14edcb96e445264ed140d3ffceaba1cea7ed200
zujorv <zujorv@users.noreply.github.com>
2017-10-28 18:47:10 +0200

    Update params.py

--- package/scripts/params.py
+++ package/scripts/params.py
@@ -3,7 +3,8 @@ from resource_management import *
 
 # server configurations
 config = Script.get_config()
-
+# constants
 content_filename = "./GFD_downloaded_files.info.json"
+# provided configuration
 content_json = config['configurations']['downloader-config']['content.json']
 log_file = config['configurations']['downloader-config']['log.file']
